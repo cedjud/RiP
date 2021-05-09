1c70234a15bfa3a69a4a71311d604f9befc6d33b
Cedric Jud <cedric@apt.no>
2021-05-09 17:29:57 +0200

    Link to frontpage from site logo

--- frontend/components/Logo/Logo.js
+++ frontend/components/Logo/Logo.js
@@ -1,7 +1,14 @@
 import React from 'react';
+import Link from 'next/link';
 
 function Logo() {
-  return <h1>Rettigherter i Psykiatrien</h1>
+  return (
+    <Link href="/">
+      <a>
+        <h1>Rettigherter i Psykiatrien</h1>
+      </a>
+    </Link>
+  );
 }
 
 export default Logo;
