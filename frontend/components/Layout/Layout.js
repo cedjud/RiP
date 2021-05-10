bee8276aaa339c575901b64fc9e9fc6cced493e0
Cedric Jud <cedric@apt.no>
2021-05-10 15:12:27 +0200

    Add some global styles

--- frontend/components/Layout/Layout.js
+++ frontend/components/Layout/Layout.js
@@ -1,19 +1,21 @@
 import Head from "next/head";
-import styles from "../../styles/Home.module.css";
+import {wrapper} from "./Layout.module.css";
 
 import PageHeader from "../PageHeader/PageHeader";
 
 function Layout({ children }) {
   return (
-    <div className={styles.container}>
+    <>
       <Head>
         <title>Rettigherter i Psykiatrien</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <PageHeader />
-      {children}
-    </div>
+      <main className={wrapper}>
+        {children}
+      </main>
+    </>
   );
 }
 
