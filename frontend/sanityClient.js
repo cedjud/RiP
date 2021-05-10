6fda9d6c8f37f52328d96491eb3e0902bfb4413c
Cedric Jud <cedric@apt.no>
2021-05-10 15:11:26 +0200

    Update config

--- frontend/sanityClient.js
+++ frontend/sanityClient.js
@@ -4,5 +4,6 @@ import sanityClient from '@sanity/client'
 export default sanityClient({
   projectId: 'w1jsruhz', // you can find this in sanity.json
   dataset: 'production', // or the name you chose in step 1
-  useCdn: true // `false` if you want to ensure fresh data
+  useCdn: false, // `false` if you want to ensure fresh data
+  apiVersion: '2021-05-10'
 })
